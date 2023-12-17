bcea737caba8388dea433e166c54bfb8692108ad
Tom Rives <tom.rives@epitech.eu>
2023-12-17 22:06:21 +0100

    [ADD] Link WeatherApp component to API Data

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import WeatherNow from "./components/WeatherNow";
 
 function App() {
   const [city, setCity] = useState("London");
-  const [weatherData, setWeatherData] = useState({});
+  const [weatherData, setWeatherData] = useState({ now: {}, futur: {} });
 
   const handleCityChange = (newCity: string) => {
     setCity(newCity);
@@ -16,9 +16,10 @@ function App() {
   const fetchWeather = async (cityName: string) => {
     try {
       const geoData = await fetchCityLocation(cityName);
-      const weatherData = await fetchWeatherData(geoData.lat, geoData.lon);
+      const now = await fetchWeatherDataNow(geoData.lat, geoData.lon);
+      const futur = await fetchWeatherData(geoData.lat, geoData.lon);
 
-      console.log(weatherData);
+      setWeatherData({ now, futur });
     } catch (error) {
       console.error("Error fetching weather data:", error);
       console.log(error);
@@ -26,7 +27,21 @@ function App() {
   };
 
   const fetchWeatherData = async (lat: Number, lon: Number) => {
-    const API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_OPENWEATHER_KEY_API}`;
+    const API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${import.meta.env.VITE_OPENWEATHER_KEY_API}`;
+    try {
+      const response = await fetch(API_URL, { method: "GET" });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const fetchWeatherDataNow = async (lat: Number, lon: Number) => {
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${import.meta.env.VITE_OPENWEATHER_KEY_API}`;
     try {
       const response = await fetch(API_URL, { method: "GET" });
       if (!response.ok) {
@@ -65,7 +80,7 @@ function App() {
         <CityWeatherForm onCityChange={handleCityChange} />
 
         <div className="weather-dashboard flex flex-wrap space-x-10 space-y-4 pb-3">
-          <WeatherNow />
+          <WeatherNow data={weatherData} />
           <div className="flex-grow">
             <TemperatureChart />
             <div className="weather-week pb-2 pr-3 py-3 flex flex-row gap-2">
--- src/components/WeatherNow.tsx
+++ src/components/WeatherNow.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const WeatherNow = () => {
+const WeatherNow = ({ data }: any) => {
 
     var [date, setDate] = useState(new Date());
 
@@ -9,20 +9,26 @@ const WeatherNow = () => {
         return () => clearInterval(timer)
     }, []);
 
+    const cityName = data?.now?.name ?? "Unknown";
+    const temperature = data?.now?.main?.temp ?? "--";
+    const humidity = data?.now?.main?.humidity ?? "N/A";
+    const windSpeed = Math.round((data?.now?.wind?.speed ?? 0) * 3.6);
+    const weather = data?.now?.weather?.[0]?.main ?? "Unknown";
+
     return (
         <div className="weather-now flex flex-col items-center justify-center pb-10 pl-4 pt-2">
             <p className="text-slate-500">{date.toDateString()} {date.getHours()}:{date.getMinutes()}</p>
-            <h3 className="text-2xl font-bold">London</h3>
-            <p className="font-bold text-3xl">42 C°</p>
-            <p className="font-bold text-2xl">Cloudy</p>
+            <h3 className="text-2xl font-bold">{cityName}</h3>
+            <p className="font-bold text-3xl">{temperature} C°</p>
+            <p className="font-bold text-2xl">{weather}</p>
             <div className="flex flex-row items-center justify-center space-x-4 text-center">
                 <div className="humidity-data" >
                     <p>Humidity</p>
-                    <p>45%</p>
+                    <p>{humidity}%</p>
                 </div>
                 <div className="wind-data">
                     <p>Wind speed</p>
-                    <p>12 km/h</p>
+                    <p>{windSpeed} km/h</p>
                 </div>
             </div>
         </div>
