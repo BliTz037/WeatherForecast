b5f7d1674c897b9412ead97323c426c582f8e6be
Tom Rives <tom.rives@epitech.eu>
2023-12-17 17:54:03 +0100

    [ADD] Date in Weather Now

--- src/components/WeatherNow.tsx
+++ src/components/WeatherNow.tsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
+
 const WeatherNow = () => {
+
+    var [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const timer = setInterval(() => setDate(new Date()), 1000)
+        return () => clearInterval(timer)
+    }, []);
+
     return (
         <div className="weather-now flex flex-col items-center justify-center pb-10 pl-4 pt-2">
-            <p className="text-slate-500">5:26 PM, Saturday, 24 November 2021</p>
+            <p className="text-slate-500">{date.toDateString()} {date.getHours()}:{date.getMinutes()}</p>
             <h3 className="text-2xl font-bold">London</h3>
             <p className="font-bold text-3xl">42 C°</p>
             <p className="font-bold text-2xl">Cloudy</p>
